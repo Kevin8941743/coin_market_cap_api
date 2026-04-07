66d5aeacd89a288f43348030e333d6a66ecf5485
Kevin thapa <bobmarley9461238@gmail.com>
2026-04-07 19:12:24 +0100

    configure environment variables and initialise app setup

--- server.js
+++ server.js
@@ -5,4 +5,11 @@ import dotenv from "dotenv"
 import mongoose from "mongoose"
 import { createClient } from "redis"
 
-
+dotenv.config()
+const app = express()
+const MongoDB = process.env.MONGO_DB
+const port = 3000
+const API_KEY = process.env.API_KEY
+const client = createClient({
+    url: process.env.REDIS_URL
+})
